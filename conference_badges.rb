66fda4ef86e445223e4e925da5bd0d4d5e8fae1f
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-15 15:28:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,10 @@ def batch_badge_creator(array)
     badges << "Hello, my name is #{name}."
   end
 end
+def assign_rooms(array)
+  rooms=[]
+  array.each do |name, index|
+    rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}"
+  end
+
+end
